5ddcfa9a53aab24392dbd572c7ba89a8596598bf
satyam singh <satyam884060singh@gmail.com>
2025-07-31 00:19:23 +0530

    re-backend

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -6,6 +6,6 @@ const router = Router();
 router.post("/login",loginController);
 router.post("/register",registerController);
 router.post("/verifyOtp",verifyOTP);
-router.get("/fetchData",)
+
 
 export default router
